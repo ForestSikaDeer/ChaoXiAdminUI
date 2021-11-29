72c2036d6d56fa678ba6667c2830ebbf87a806ec
LangGuoYu <supercutcat@outlook.com>
2021-11-29 17:43:54 +0800

    Rebuild ChaoXi UI

--- src/main.js
+++ src/main.js
@@ -1,34 +1,4 @@
-import Vue from 'vue'
+import { createApp } from 'vue'
+import App from './App.vue'
 
-import 'normalize.css/normalize.css' // A modern alternative to CSS resets
-
-
-
-import locale from 'element-ui/lib/locale/lang/en' // lang i18n
-
-import '@/styles/index.scss' // global css
-
-import App from './App'
-import router from './router'
-import store from './store'
-
-import '@/icons' // icon
-import '@/permission' // permission control
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-// main.js
-import * as echarts from "echarts"
-Vue.prototype.$echarts = echarts
-import Antd from 'ant-design-vue'
-import 'ant-design-vue/dist/antd.css'
-Vue.use(Antd)
-Vue.use(ElementUI, { locale })
-
-Vue.config.productionTip = false
-
-new Vue({
-  el: '#app',
-  router,
-  store,
-  render: h => h(App)
-})
+createApp(App).mount('#app')
--- src/views/activity/allocation.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="app-container">
-<a-empty />
-</div>
-      </template>
